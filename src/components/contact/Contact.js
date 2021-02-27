b471f813b78091fd047ddd985242d15922836338
Alec Springel <alecspringel@gmail.com>
2021-02-26 16:07:23 -0800

    Added media queries to welcome and contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -7,14 +7,14 @@ import Email from "../../images/contact/email.svg";
 
 const Contact = () => {
   return (
-    <Section className="section">
+    <Section className="section" id="contact">
       <h3 className="heading-color">Contact</h3>
       <Description className="margin-tb20">
         Thanks for stopping by! I always enjoy meeting fellow developers and
         hearing about new projects. Feel free to contact me by text, phone, or
         email and I will get back to you as soon as possible.
       </Description>
-      <div className="flex-row" style={{ marginTop: 80 }}>
+      <FlexWrapper className="flex-row" style={{ marginTop: 80 }}>
         <ContactCard>
           <Icon src={Phone} />
           <h6 className="heading-color">Cell Phone</h6>
@@ -35,7 +35,7 @@ const Contact = () => {
           <h6 className="heading-color">GitHub</h6>
           <p className="primary-color">@alecspringel</p>
         </ContactCard>
-      </div>
+      </FlexWrapper>
     </Section>
   );
 };
@@ -46,19 +46,27 @@ const Section = styled.section`
   text-align: center;
 `;
 
+const FlexWrapper = styled.div`
+  flex-wrap: wrap;
+`;
+
 const ContactCard = styled.a`
   flex: 1;
   background-color: ${(props) => props.theme.background2};
-  margin: 0 20px;
+  margin: 20px;
   border-radius: 6px;
   box-shadow: 0px 4.59773px 22.9887px rgba(0, 0, 0, 0.25);
-  padding: 30px;
+  padding: 40px;
+  width: 226px;
+  @media (max-width: 1256px) {
+    flex: 40%;
+  }
 `;
 
 const Icon = styled.img`
   height: 34px;
   width: 34px;
-  margin: 40px 0;
+  margin: 25px 0;
 `;
 
 const Description = styled.p`
--- src/components/home/Welcome.js
+++ src/components/home/Welcome.js
@@ -7,27 +7,25 @@ import BrowserFigure from "./BrowserFigure";
 const Welcome = () => {
   return (
     <SectionWrapper className="flex-row">
-      <div style={{ flex: 1.1 }}>
-        <h6 className="primary-color margin-b10">Hi, I'm</h6>
-        <h1 className="heading-color margin-b10">Alec Springel.</h1>
-        <h3 className="margin-b20">Full-stack software engineer.</h3>
-        <p style={{ maxWidth: 483 }}>
-          A full-stack engineer with a passion for creating solutions to
-          everyday problems. I enjoy building, designing, and deploying scalable
-          software.
-        </p>
-        <Button style={{ marginTop: 50 }} width="120px">
-          Say Hello
-        </Button>
+      <div style={{ flex: 1.1 }} className="flex-row align">
+        <div>
+          <h6 className="primary-color margin-b10">Hi, I'm</h6>
+          <h1 className="heading-color margin-b10">Alec Springel.</h1>
+          <h3 className="margin-b20">Full-stack software engineer.</h3>
+          <p style={{ maxWidth: 483 }}>
+            A full-stack engineer with a passion for creating solutions to
+            everyday problems. I enjoy building, designing, and deploying
+            scalable software.
+          </p>
+          <Button style={{ marginTop: 50 }} width="120px">
+            Say Hello
+          </Button>
+        </div>
       </div>
-      <div style={{ flex: 1 }}>
-        <FigureWrapper>
-          <Figure>
-            <CodeWindow src={CodeWindowSvg} />
-            <BrowserFigure />
-          </Figure>
-        </FigureWrapper>
-      </div>
+      <Figure>
+        <CodeWindow src={CodeWindowSvg} />
+        <BrowserFigure />
+      </Figure>
     </SectionWrapper>
   );
 };
@@ -36,14 +34,27 @@ export default Welcome;
 
 const SectionWrapper = styled.section`
   padding: 170px 0;
+  min-height: 90vh;
+  transform: scale(1.2);
+  padding-top: 0;
+
+  @media (max-width: 1560px) {
+    transform: scale(1);
+  }
+  @media (max-width: 989px) {
+    flex-direction: column-reverse;
+    align-items: center;
+  }
 `;
 
 const Figure = styled.figure`
+  display: flex;
+  align-items: center;
+  flex: 1;
   position: relative;
-`;
-
-const FigureWrapper = styled.div`
-  position: absolute;
+  @media (max-width: 1060px) {
+    transform: scale(0.8);
+  }
 `;
 
 const CodeWindow = styled.img`
